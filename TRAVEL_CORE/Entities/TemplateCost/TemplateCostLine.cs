700461c5a30376d7830e3393150be7af34f5e4e8
Shahin9797 <shahin.mustafayev@uniser.az>
2022-11-11 15:38:37 +0400

    shahin

--- TRAVEL_CORE/Entities/TemplateCost/TemplateCostLine.cs
+++ TRAVEL_CORE/Entities/TemplateCost/TemplateCostLine.cs
@@ -4,12 +4,14 @@
     {
         public string? Vender { get; set; }
         public int VenderService { get; set; }
+        public string? VenderServiceName { get; set; }
         public int Qty { get; set; }
         public float VenderUnitPrice { get; set; }
         public decimal VenderAmount { get; set; }
         public decimal SaleUnitPrice { get; set; }
         public decimal SaleAmount { get; set; }
         public int Currency { get; set; }
+        public string? CurrencyName { get; set; }
         public float CurrencyRate { get; set; }
         public decimal CurrencyAmount { get; set; }
     }
