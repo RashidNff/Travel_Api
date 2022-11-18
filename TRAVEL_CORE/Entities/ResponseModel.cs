dcc5b838f6f209f8c86877ddc4793eae7a3c77f0
Shahin9797 <shahin.mustafayev@uniser.az>
2022-11-18 12:05:59 +0400

    shahin

--- TRAVEL_CORE/Entities/ResponseModel.cs
+++ TRAVEL_CORE/Entities/ResponseModel.cs
@@ -4,6 +4,6 @@
     {
         public string? Message { get; set; }
         public bool? Status { get; set; }
-        public int? Data { get; set; }
+        public object? Data { get; set; }
     }
 }
